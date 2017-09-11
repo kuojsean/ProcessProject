af17c4d5d733506c355b60a428582256f343893d
kuojsean <kuo.j.sean@gmail.com>
2017-09-11 08:32:17 -0600

    Created PlayDohCircle and updated controler to create PlayDohCirlce instance

--- src/sample/controller/SampleController.java
+++ src/sample/controller/SampleController.java
@@ -1,11 +1,38 @@
 package sample.controller;
 
+import java.util.Scanner;
+import sample.model.PlayDohCircle;
+
 public class SampleController
 {
-
+	public SampleController()
+	{
+		
+	}
+	
 	public void start()
 	{
+		PlayDohCircle firstCircle = new PlayDohCircle();
+		PlayDohCircle secondCircle;
+		secondCircle = new PlayDohCircle();
+		
+		PlayDohCircle thirdCircle;
+		thirdCircle = new PlayDohCircle(5);
+		
+		
+		
 		System.out.println("This is a sample project to show the github and");
 		System.out.println("eclipse process so I develop muscle memory :D");
+	
+		testScanner();
+	}
+	
+	private void testScanner()
+	{
+		Scanner firstScanner; //Declares a Scanner Object.
+		firstScanner = new Scanner(System.in); //Instantiates a Scanner Object.
+		System.out.println("Please type your favorite food");
+		String answer = firstScanner.nextLine();
+		System.out.println("Oh, you like to eat " + answer);
 	}
 }
---
+++ src/sample/model/PlayDohCircle.java
@@ -0,0 +1,20 @@
+package sample.model;
+
+public class PlayDohCircle
+{
+	//Declaration section
+	//Data member section
+	//NEED TO BE PRIVATE
+	private int size;
+	
+	public PlayDohCircle()
+	{
+		this.size = 5;
+	}
+	
+	public PlayDohCircle(int size)
+	{
+		this.size = size;
+	}
+	
+}
