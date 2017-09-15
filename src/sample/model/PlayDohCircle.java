25c7af1f52a6e3c939bd20f9d67156c73fda073c
kuojsean <kuo.j.sean@gmail.com>
2017-09-15 07:44:08 -0600

    Created a change size input in my code.

--- src/sample/model/PlayDohCircle.java
+++ src/sample/model/PlayDohCircle.java
@@ -17,6 +17,11 @@ public class PlayDohCircle
 		this.size = size;
 	}
 	
+	public void setSize(int updateSize)
+	{
+		this.size = updateSize;
+	}
+	
 	public String toString()
 	{
 		String description = "I am a PlayDohCircle and my size is " + size;
